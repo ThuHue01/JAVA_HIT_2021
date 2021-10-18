e6935cedd963e5490206138d6b486330e5766083
ThuHue01 <huechuyenbg@gmail.com>
2021-10-18 15:50:11 +0700

    BTVN B1

---
+++ Buoi1/src/Bai1.java
@@ -0,0 +1,9 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 5;
+        System.out.println("Gia tri của m la: " + m);
+        System.out.println("Gia tri của n la: " + n);
+    }
+}
+
---
+++ Buoi1/src/Bai2.java
@@ -0,0 +1,21 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        System.out.println("*       *      *      * * * *    * * * *   *     *");
+        System.out.println("|       |    /   \\    |       *  |       *  \\   /");
+        System.out.println("* ----- *   * --- *   * * * *    * * * *      *");
+        System.out.println("|       |  /       \\  |          |            |");
+        System.out.println("*       * *         * *          *            *");
+        System.out.println("\n");
+        System.out.println("*   * ****** *       *        *");
+        System.out.println("|\\  | |       \\     / \\     /");
+        System.out.println("* * * ******   *   *   *   *");
+        System.out.println("|  \\| |         \\ /     \\ /");
+        System.out.println("*   * ******     *       *");
+        System.out.println("\n");
+        System.out.println("*     * ******      *      * * * *");
+        System.out.println(" \\   /  |         /   \\    |       *");
+        System.out.println("   *    ******   * --- *   * * * *");
+        System.out.println("   |    |       /       \\  |    \\ ");
+        System.out.println("   *    ****** *         * *     *");
+    }
+}
